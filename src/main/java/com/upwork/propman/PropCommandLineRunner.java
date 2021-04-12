379e82e94b8c4f567301844ffe917a07435df9c0
imran ansari <stasim101@gmail.com>
2021-04-12 16:44:19 +0530

    adding images

--- src/main/java/com/upwork/propman/PropCommandLineRunner.java
+++ src/main/java/com/upwork/propman/PropCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.upwork.propman.repository.ApiKeyRepository;
+import com.upwork.propman.repository.PropertyRepository;
 import com.upwork.propman.service.ApiKeyService;
 
 @Component
@@ -12,9 +14,17 @@ public class PropCommandLineRunner implements CommandLineRunner {
 	@Autowired
 	private ApiKeyService apiKeyService;
 
+	@Autowired
+	private ApiKeyRepository apiKeyRepository;
+
+	@Autowired
+	private PropertyRepository propertyRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
+		apiKeyRepository.deleteAll();
+		propertyRepository.deleteAll();
 		apiKeyService.storeKey("admin", "4F6D83MH53");
 
 	}
